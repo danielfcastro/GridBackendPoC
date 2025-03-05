b0a3d4a36fc65c03f64e099d9b5d3d40e194be91
Daniel (D.) Ferreira Castro <daniel.ferreira.castro@ing.com>
2025-03-05 19:36:54 +0000

    fix for schema

--- src/main/resources/db/flyway/V1__CREATE_SCHEMA.sql
+++ src/main/resources/db/flyway/V1__CREATE_SCHEMA.sql
@@ -5,18 +5,9 @@ BEGIN
         END IF;
 END $$;
 
-CREATE TABLE IF NOT EXISTS test.alt_orgx (
-                          alt_org_id BIGINT NOT NULL,
-                          org_id INT NOT NULL,
-                          alt_sys_cd  CHAR(10),
-                          alt_ref VARCHAR(40),
-                          alt_ref_type CHAR(10),
-                          trigger_autogen_cascade BOOLEAN,
-                          tms_control_id BIGINT NOT NULL,
-                          version_num SMALLINT,
-                          sys_user VARCHAR(60),
-                          sys_dt TIMESTAMP,
-                          proposal_user VARCHAR(60),
-                          proposal_dt TIMESTAMP,
-                          created_by_role BOOLEAN
+CREATE TABLE IF NOT EXISTS test.myTable (
+                          id BIGINT NOT NULL,
+                          age INT NOT NULL,
+                          name  CHAR(10),
+                          description VARCHAR(40)
 );
